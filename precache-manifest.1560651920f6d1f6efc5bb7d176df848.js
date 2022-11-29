0e68ba1b5d18d74f5b276e6587416b161f133126
IMxSPYDER <hostingpurposee@gmailcom>
2022-11-29 12:24:32 +0530

    Updates

---
+++ precache-manifest.1560651920f6d1f6efc5bb7d176df848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ad55b631c437228ca92",
+    "url": "/TextEditor/static/css/main.1e0152db.chunk.css"
+  },
+  {
+    "revision": "1ad55b631c437228ca92",
+    "url": "/TextEditor/static/js/main.1ad55b63.chunk.js"
+  },
+  {
+    "revision": "7b2e37571c17e8d4fdd1",
+    "url": "/TextEditor/static/js/1.7b2e3757.chunk.js"
+  },
+  {
+    "revision": "9a086d25258b1ecdec48",
+    "url": "/TextEditor/static/js/2.9a086d25.chunk.js"
+  },
+  {
+    "revision": "da0e01c2384c7c9c99ff",
+    "url": "/TextEditor/static/js/runtime~main.da0e01c2.js"
+  },
+  {
+    "revision": "e274f564887e0ea67247c8b0d3301354",
+    "url": "/TextEditor/index.html"
+  }
+];
